9205f260ae583781a35007e3410b3c6272ed6582
go <gustaf@oxenstierna.org>
2021-12-03 00:03:03 +0100

    Day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,25 @@ fn day1() -> u32 {
     count
 }
 
+fn day1_pt2() -> u32 {
+    let mut lines = Vec::new();
+    let contents = fs::read_to_string("input_day1.txt").expect("Can't read");
+    for line in contents.lines() {
+        lines.push(line.parse::<u32>().unwrap())
+    }
+    let mut previous = u32::MAX;
+    let mut count = 0;
+    for w in lines.as_slice().windows(3) {
+        let window_sum = w.iter().sum::<u32>();
+        if window_sum > previous {
+            count += 1;
+        }
+        previous = window_sum;
+    }
+    count
+}
+
 fn main() {
-    println!("Final count {}", day1());
+    println!("Day1 problem {}", day1());
+    println!("Day 2 problem {}", day1_pt2());
 }
