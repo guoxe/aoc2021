afba2f2eac5f20d848077e7333c0b971104dd4c5
go <gustaf@oxenstierna.org>
2021-12-06 00:13:22 +0100

    Day3 pt 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn day1() -> u32 {
@@ -67,9 +68,55 @@ fn day2_pt2() -> u32 {
     longitude * latitude
 }
 
+fn day3_pt1() -> u32 {
+    let contents = fs::read_to_string("input_day3.txt").expect("Can't read");
+    let mut counts = HashMap::new();
+    for line in contents.lines() {
+        for (position, value) in line.chars().enumerate() {
+            let value = value.to_digit(10).unwrap();
+            *counts.entry((position, value)).or_insert(0) += 1;
+        }
+    }
+    let mut gamma_binary = Vec::new();
+    for i in 0..12 {
+        let num_ones = *counts.get(&(i, 1)).unwrap();
+        let num_zeros = *counts.get(&(i, 0)).unwrap();
+        if num_ones > num_zeros {
+            gamma_binary.push(1);
+        } else {
+            gamma_binary.push(0);
+        }
+    }
+    let gamma = u32::from_str_radix(
+        &gamma_binary
+            .clone()
+            .into_iter()
+            .map(|i| i.to_string())
+            .collect::<String>(),
+        2,
+    )
+    .unwrap();
+    let epsilon = u32::from_str_radix(
+        &gamma_binary
+            .clone()
+            .into_iter()
+            .map(|i| match i {
+                0 => 1,
+                1 => 0,
+                _ => -1,
+            })
+            .map(|i| i.to_string())
+            .collect::<String>(),
+        2,
+    )
+    .unwrap();
+    gamma * epsilon
+}
+
 fn main() {
     //println!("Day1 problem {}", day1());
     //println!("Day 2 problem {}", day1_pt2());
-    println!("{}", day2_pt1());
-    println!("{}", day2_pt2());
+    //println!("{}", day2_pt1());
+    //println!("{}", day2_pt2());
+    println!("{}", day3_pt1());
 }
